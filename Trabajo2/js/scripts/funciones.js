19246725a771384cf639b36f2d1fb88df45b8137
Olano-Lautaro <lautarofcbgg@gmail.com>
2022-04-12 12:14:22 -0300

    funciones

---
+++ Trabajo2/js/scripts/funciones.js
@@ -0,0 +1,15 @@
+function Hola(){
+    alert('Hola')
+}
+
+function Chau(){
+    alert('Chau');
+}
+
+function pasar(){
+    alert('Pasaste por aca')
+}
+
+function cambiar(){
+    document.getElementById("A1").style.color="black"; document.getElementById("A1").style.backgroundColor="Yellow";
+}
