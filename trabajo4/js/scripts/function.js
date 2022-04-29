129e2be44f2f26d407d2c28e3cd04887a30dfb9c
Olano-Lautaro <lautarofcbgg@gmail.com>
2022-04-29 16:51:10 -0300

    Arrays

--- trabajo4/js/scripts/function.js
+++ trabajo4/js/scripts/function.js
@@ -18,7 +18,7 @@
 
     //------Otro tipo de recorrer el array------//
 
-    miarray.forEach(Element => {
+    miarray.forEach(Element =>{
         console.log(miarray[Element]);
     });
 
@@ -27,21 +27,28 @@
     miarray.push('Sandia');
     miarray.unshift('Naranja');
     miarray.pop('Pera');
-    console.log(miarray);
-      
+    console.log(miarray);     
+    console.log("*---------------------------------------------*");
 
-    //------------------------------------------//    
+    //----------------Otro ejercicio con array---------------------//    
   
     let array=["99","4","34","93","1"];
-    
-
+ function Array_(){
+    console.log("Números mayores a 50:")  
     for (let i=0; i<array.length; i++){
-    
+        
         if (array[i]>50){
             console.log(array[i])
         }
     }
 
+    console.log("Números Pares:")
+    for (let i=0; i<array.length; i++){
+        if (array[i]%2==0){
+            console.log(array[i]);
+        }
+    }
+  }  
 
     
     
