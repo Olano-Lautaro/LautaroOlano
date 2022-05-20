94588982dab5de02a0ab19d951615c5133e084ab
Olano-Lautaro <lautarofcbgg@gmail.com>
2022-05-20 19:22:24 -0300

    funciones trabajo8

--- trabajo8/fuctions.js
+++ trabajo8/fuctions.js
@@ -8,8 +8,9 @@ function    Usuario(){
     fetch   ('https://randomuser.me/api/')
     .then   (res    => res.json())
     .then   (data   =>{
+        console.log(data.results[0]);
         let años=data.results[0].dob.age;
-
+        
         if  (data.results[0].gender=="female" && años>20 && años<40){
 
             document.getElementById("usuario").innerHTML=("usuario mujer");
@@ -23,19 +24,26 @@ function    Usuario(){
                 Edad: ${data.results[0].dob.age}<br>
                 Email: ${data.results[0].email}<br>
                 Foto: <br>
-                <img src="${data.results[0].picture.large}">
-                Longuitud: ${data.results[0]}` 
+                <img src="${data.results[0].picture.large}"><br>
+                Longuitud: ${data.results[0].location.coordinates.longitude}<br>
+                Latitud: ${data.results[0].location.coordinates.latitude}<br><hr>` 
                 document.getElementById("edad").innerHTML=("cumple con la edad");
                 document.getElementById("contenido").innerHTML= HTML;
             //}
             //else{
                 //alert('Usuario no cumple con los requisitos, fuera del rango de edad');
             //}
+            var map = L.map('map').setView([data.results[0].location.coordinates.latitude, data.results[0].location.coordinates.longitude], 13);
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+            }).addTo(map);
+            var marker = L.marker([data.results[0].location.coordinates.latitude, data.results[0].location.coordinates.longitude]).addTo(map);
     
+           
         }else (data.results[0].gender=="male");{
           alert('Usuario no cumple con los requisitos, es masculino');
         }
-
+           
         
         
     })
