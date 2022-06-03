7c388223c6cfac4d21a2500f0794db822497f3f0
Olano-Lautaro <lautarofcbgg@gmail.com>
2022-06-02 21:21:08 -0300

    php

--- trabajo9/index.php
+++ trabajo9/index.php
@@ -1,20 +1,27 @@
 <?php
-  //$contenido=array(
+  
   $apellido=$_POST['apellido'];
   $nombre=$_POST['nombre'];
   $dni=$_POST['DNI'];
   $nacimiento=$_POST['fecha_nac'];
   $sexo=$_POST['sexo'];
-  $colores=array();
-  //);
+  
   echo 'Apellido: '.$apellido.'<br>';
   echo  'Nombre: '.$nombre.'<br>';
   echo 'DNI: '.$dni.'<br>';
   echo 'Nacimiento: '.$nacimiento.'<br>';
   echo 'Sexo: '.$sexo.'<br>';
+
+  echo 'Colores: ';  
+  if(isset($_POST['colores'])){
+    $color=$_POST['colores'];
+    for($i=0; $i<count($color); ++$i){
+      print_r(' '.$color[$i].'  ');
+    };
+  }else{
+    echo '(No seleccionó nignún color)';
+  };
   
-  (isset($_POST['verde'])){
-    array_push($colores, $_POST['verde'])};
   
     
 ?>
