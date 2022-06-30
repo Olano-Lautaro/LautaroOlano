47088727322bc53a56874d8d0ec9eadc35b26553
Olano-Lautaro <lautarofcbgg@gmail.com>
2022-06-30 19:43:47 -0300

    parcial Nº1

--- parcial programacion/functions.js
+++ parcial programacion/functions.js
@@ -3,84 +3,108 @@ let socio1={
     apellido:   "Parra",
     nombre: "Javier",
     edad:   41,
-    dni:28358603,
-    facturas:1
+    dni:1,
+    facturas:3
 }
-//console.log(socio1)
+
 
 let socio2={
     apellido:   "Olano",
     nombre: "Lautaro",
     edad:  20,
-    dni:43828075,
+    dni:2,
     facturas: 5
 }
-//console.log(socio2)
+
 
 let socio3={
     apellido:   "Perez",
     nombre: "Gabriel",
     edad:   33,
-    dni:25902133,
+    dni:3,
     facturas:   6
 }
-//console.log(socio3)
+
 
 let socio4={
     apellido:   "Sanchez",
     nombre: "Juana",
     edad:23,
-    dni: 33910984,
+    dni: 4,
     facturas:   20
 }
-//console.log(socio4)
+
 
 let socio5={
     apellido:   "Romero",
     nombre:"Victoria",
     edad:   50,
-    dni:34567005,
+    dni:5,
     facturas:   7
 }
-//console.log(socio5)
+
 
 function buscar_dni(){
-    //console.log(document.getElementById('dni').value);
+    
     let socios=[socio1,socio2,socio3,socio4,socio5]
 
     for (let i=0; i<(socios.length); i++){
 
-        let cant_factura= socios[i].facturas
+        
+        let dni_buscado= document.getElementById('dni').value
+        
+        let flag=false
 
-        if (socios[i].dni == document.getElementById('dni').value){
-            console.log(socios[i].facturas)
-            //console.log('Entro');
+        if (socios[i].dni==dni_buscado){
+            console.log('Cantidad de facturas del socio: '+socios[i].facturas)
+            console.log('Entro')
+            
             //MENOS O IGUAL A 3 FACTURAS
-            if (cant_factura<=3){
+            if(socios[i].facturas<=3){
                 document.getElementById('cartel').style.background="green"
-                document.getElementById('cartel').innerHTML= 'Facturas: '+ cant_factura
+                document.getElementById('cartel').innerHTML= 'Facturas: '+ socios[i].facturas
                 document.getElementById('info_socio').innerHTML= 'Nombre Completo del Socio: ' + socios[i].apellido+', '+socios[i].nombre
-            };
+            }
+
             // ENTRE 3 A 6 FACTURAS
-            if (cant_factura>3 & cant_factura<6){
+            if(socios[i].facturas>3 & socios[i].facturas<6){
                 document.getElementById('cartel').style.background="yellow"
-                document.getElementById('cartel').innerHTML= 'Facturas: '+ cant_factura
+                document.getElementById('cartel').innerHTML= 'Facturas: '+ socios[i].facturas
                 document.getElementById('info_socio').innerHTML= 'Nombre Completo del Socio: ' + socios[i].apellido+', '+socios[i].nombre
-            };
+            }
+
             // MAYOR O IGUAL A 6 FACURAS
-            if (cant_factura<=6){
+            if(socios[i].facturas>=6){
                 document.getElementById('cartel').style.background="red"
-                document.getElementById('cartel').innerHTML= 'Facturas: '+ cant_factura
+                document.getElementById('cartel').innerHTML= 'Facturas: '+ socios[i].facturas
                 document.getElementById('info_socio').innerHTML= 'Nombre Completo del Socio: ' + socios[i].apellido+', '+socios[i].nombre
-            };
+            }
 
-        }//else{
+            console.log(socios[i]);
+            return flag=true
+        }
+
+        
+
+        if (flag==false){
+            console.log(flag)
+
+            console.log(i)
             document.getElementById('cartel').style.background="red"
-            document.getElementById('cartel').innerHTML= "No Existe"
-        //}  
+            document.getElementById('cartel').innerHTML= "No existe socio"
+    
+        }
+           
+        
+        
+        
+            
+        
 
-        console.log(socios[i])
-    }
+        
+
+        
+    };
 }
         
 
