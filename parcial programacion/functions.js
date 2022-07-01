829bc6acedbe9ae32b1dbc4900bdab74bf72aab5
Olano-Lautaro <lautarofcbgg@gmail.com>
2022-06-30 21:16:56 -0300

    parcial Nº1

--- parcial programacion/functions.js
+++ parcial programacion/functions.js
@@ -1,112 +1,131 @@
 
-let socio1={
-    apellido:   "Parra",
+let socio1 = {
+    apellido: "Parra",
     nombre: "Javier",
-    edad:   41,
-    dni:1,
-    facturas:3
+    edad: 41,
+    dni: 1,
+    facturas: 3
 }
 
 
-let socio2={
-    apellido:   "Olano",
+let socio2 = {
+    apellido: "Olano",
     nombre: "Lautaro",
-    edad:  20,
-    dni:2,
+    edad: 20,
+    dni: 2,
     facturas: 5
 }
 
 
-let socio3={
-    apellido:   "Perez",
+let socio3 = {
+    apellido: "Perez",
     nombre: "Gabriel",
-    edad:   33,
-    dni:3,
-    facturas:   6
+    edad: 33,
+    dni: 3,
+    facturas: 6
 }
 
 
-let socio4={
-    apellido:   "Sanchez",
+let socio4 = {
+    apellido: "Sanchez",
     nombre: "Juana",
-    edad:23,
+    edad: 23,
     dni: 4,
-    facturas:   20
+    facturas: 20
 }
 
 
-let socio5={
-    apellido:   "Romero",
-    nombre:"Victoria",
-    edad:   50,
-    dni:5,
-    facturas:   7
+let socio5 = {
+    apellido: "Romero",
+    nombre: "Victoria",
+    edad: 50,
+    dni: 5,
+    facturas: 7
 }
 
 
-function buscar_dni(){
-    
-    let socios=[socio1,socio2,socio3,socio4,socio5]
+function buscar_dni() {
+    //Array Socios
+    let socios = [socio1, socio2, socio3, socio4, socio5]
 
-    for (let i=0; i<(socios.length); i++){
+    // facturas
+    let factura_verde = 3
+    let factura_roja = 6
 
-        
-        let dni_buscado= document.getElementById('dni').value
-        
-        let flag=false
 
-        if (socios[i].dni==dni_buscado){
-            console.log('Cantidad de facturas del socio: '+socios[i].facturas)
-            console.log('Entro')
-            
+    for (let i = 0; i < (socios.length); i++) {
+
+        //DNI a buscar
+        let dni_buscado = document.getElementById('dni').value
+
+        //dni del socio
+        let dni_socio = socios[i].dni
+
+        let flag = false
+
+
+        if (dni_socio == dni_buscado) {
+
+            // Cartel con informacion de facturas
+            let info_facturas = document.getElementById('cartel').innerHTML = 'Facturas: ' + socios[i].facturas
+
+            // cartel con informacion del socio
+            let info_socio = document.getElementById('info_socio').innerHTML = 'Nombre Completo del Socio: ' + socios[i].apellido + ', ' + socios[i].nombre
+
+
+            console.log('Posición de array: ' + i + ' Entro');
+
+            console.log('Cantidad de facturas del socio: ' + socios[i].facturas);
+
+            info_socio;
+
+            info_facturas;
+
             //MENOS O IGUAL A 3 FACTURAS
-            if(socios[i].facturas<=3){
-                document.getElementById('cartel').style.background="green"
-                document.getElementById('cartel').innerHTML= 'Facturas: '+ socios[i].facturas
-                document.getElementById('info_socio').innerHTML= 'Nombre Completo del Socio: ' + socios[i].apellido+', '+socios[i].nombre
+            if (socios[i].facturas <= factura_verde) {
+
+                document.getElementById('cartel').style.background = "green"
+
             }
 
             // ENTRE 3 A 6 FACTURAS
-            if(socios[i].facturas>3 & socios[i].facturas<6){
-                document.getElementById('cartel').style.background="yellow"
-                document.getElementById('cartel').innerHTML= 'Facturas: '+ socios[i].facturas
-                document.getElementById('info_socio').innerHTML= 'Nombre Completo del Socio: ' + socios[i].apellido+', '+socios[i].nombre
+            if (socios[i].facturas > factura_verde & socios[i].facturas < factura_roja) {
+
+                document.getElementById('cartel').style.background = "yellow"
+
             }
 
             // MAYOR O IGUAL A 6 FACURAS
-            if(socios[i].facturas>=6){
-                document.getElementById('cartel').style.background="red"
-                document.getElementById('cartel').innerHTML= 'Facturas: '+ socios[i].facturas
-                document.getElementById('info_socio').innerHTML= 'Nombre Completo del Socio: ' + socios[i].apellido+', '+socios[i].nombre
+            if (socios[i].facturas >= factura_roja) {
+
+                document.getElementById('cartel').style.background = "red"
+
             }
 
-            console.log(socios[i]);
-            return flag=true
+            console.log(socios[i])
+
+            flag=true 
+
+            return flag;
         }
 
-        
+        if (!flag) {
 
-        if (flag==false){
-            console.log(flag)
+            console.log('Posición de array :' + i + ' no entró')
+            document.getElementById('cartel').style.background = "red"
+            document.getElementById('cartel').innerHTML = "No existe socio"
 
-            console.log(i)
-            document.getElementById('cartel').style.background="red"
-            document.getElementById('cartel').innerHTML= "No existe socio"
-    
         }
-           
-        
-        
-        
-            
-        
 
-        
 
-        
-    };
+    }
+
+
+
+
 }
-        
+
+
 
 
 
