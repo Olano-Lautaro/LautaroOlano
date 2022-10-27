85636155edc8591831dee182963fcd093440c8ed
Olano-Lautaro <lautarofcbgg@gmail.com>
2022-10-27 19:28:02 -0300

    falta acomodar

--- parcial2/principal.php
+++ parcial2/principal.php
@@ -3,12 +3,18 @@
     require ('Persona.php');
     require ('CuentaJoven.php');
 
+    $nombre=$_POST['nombre'];
+    $apellido=$_POST['apellido'];
+    $edad=$_POST['edad'];
+    $dni=$_POST['dni'];
+    $monto=$_POST['monto'];
+
+
     //Construye Objeto Persona
-    $persona= new Persona('Lautaro','21',43828075);
+    $persona= new Persona($nombre,$edad,$dni);
     echo $persona->mostrar();
 
-    $nombre= $persona->nombre;
-    $edad= $persona->edad;
+   
 
 
     //toma True o False para calcular si el Titular tiene la edad para tener una Cuenta Joven
