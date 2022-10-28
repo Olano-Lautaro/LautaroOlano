33e287482e4039c85374c2fe5b7b4b205937cf5e
Olano-Lautaro <102390354+Olano-Lautaro@users.noreply.github.com>
2022-10-27 22:10:15 -0300

    Delete Trabajo12 POO directory

--- Trabajo12 POO/function.js
+++
@@ -1,8 +0,0 @@
-
-function tipoEmpleado(value){
-    if (value=='Repartidor'){
-       document.getElementById('seguntipo').innerHTML='Zona: <input type="text" name="zona" id="ZONA"></input>';
-    }else{
-        document.getElementById('seguntipo').innerHTML='Comisión: <input type="number" name="comision" id="COMISION"></input>'
-    }
-}
